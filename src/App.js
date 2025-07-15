e0e651a93ca2d428f05b6c0cf8e06b08d7a59376
Jung-Hwan Park <jungpark.2002@gmail.com>
2025-07-15 16:33:31 -0600

    App.js changed to reflect the page structure change

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./index.css";
-import MainEmpty from "./pages/MainEmpty";
-import MainNoteEdit from "./pages/MainNoteEdit";
-import MainNoteView from "./pages/MainNoteView";
+import Homepage from "./pages/Homepage";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/*" element={<MainEmpty />}></Route>
-        <Route path="/notes/:id" element={<MainNoteView />}></Route>
-        <Route path="/notes/:id/edit" element={<MainNoteEdit />}></Route>
+        <Route path="/*" element={<Homepage mainView = "empty"/>}></Route>
+        <Route path="/notes/:id" element={<Homepage  mainView = "viewonly"/>}></Route>
+        <Route path="/notes/:id/edit" element={<Homepage mainView = "edit"/>}></Route>
       </Routes>
     </BrowserRouter>
   );
